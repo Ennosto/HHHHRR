b61b68ecee6d19926e8b7756238f0df56ad41c25
Ennosto <ennosto_me@mail.ru>
2024-08-09 17:26:37 +0300

    Add files via upload

---
+++ ррРРР.py
@@ -0,0 +1,22 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = list({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
+students.sort()
+a = grades[0]
+b = grades[1]
+c = grades[2]
+d = grades[3]
+e = grades[4]
+a = sum(a) / len(a)
+b = sum(b) / len(b)
+c = sum(c) / len(c)
+d = sum(d) / len(d)
+e = sum(e) / len(e)
+q = students[0]
+w = students[1]
+r = students[2]
+t = students[3]
+y = students[4]
+FFFF = {q:a, w:b, r:c, t:d, y:e}
+print(FFFF)
+print(type(FFFF))
